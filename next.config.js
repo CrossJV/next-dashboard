71ef338e5c49b0d8072f15ac3778a0a02b30ed48
CrossJV <crossjvj@gmail.com>
2024-04-11 12:51:34 +0300

    refactor

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        AUTH_SECRET: 'q6dRRgZPEbqTDcoG2CrVn22c1xMUJN0wzlyVOQGKros='
+    }
+};
 
 module.exports = nextConfig;
